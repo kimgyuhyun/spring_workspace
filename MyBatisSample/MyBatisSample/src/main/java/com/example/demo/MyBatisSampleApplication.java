f7fee77100bb0964ae9d5d4ad3ba2f7756dd18c2
Kimgyuhyun <kgh980605@gmail.com>
2025-03-28 15:47:14 +0900

    Test code implemnenting for one-to-one and ont-to-many relationships by writing resultMap in MyBatis XML file

--- MyBatisSample/MyBatisSample/src/main/java/com/example/demo/MyBatisSampleApplication.java
+++ MyBatisSample/MyBatisSample/src/main/java/com/example/demo/MyBatisSampleApplication.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.demo.mapper")
 public class MyBatisSampleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyBatisSampleApplication.class, args);
 	}
-
 }
